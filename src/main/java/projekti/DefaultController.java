6e1c3fc82b508c3d118907dbd606c4d462b70305
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-10-04 18:09:07 +0300

    Set checkstyle, remove target from git

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("*")
-    public String helloWorld(Model model) {
-        model.addAttribute("message", "World!");
-        return "index";
-    }
+  @GetMapping("*")
+  public String helloWorld(Model model) {
+    model.addAttribute("message", "World!");
+    return "index";
+  }
 }
