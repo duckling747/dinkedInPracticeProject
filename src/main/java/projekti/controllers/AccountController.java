689665e81eb5964833fdc0def90d13111ca09448
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-10-20 13:56:26 +0300

    Another test

--- src/main/java/projekti/controllers/AccountController.java
+++ src/main/java/projekti/controllers/AccountController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import projekti.models.Account;
@@ -21,6 +23,7 @@ import projekti.repositories.AccountRepository;
 import projekti.repositories.FriendRequestRepository;
 
 @RestController
+@RequestMapping(consumes = { MediaType.APPLICATION_JSON_VALUE })
 public class AccountController {
 
   @Autowired
